8be7f6a2e6c7b83dc26e92350c8f4ded393bc1eb
Alessandro Beneggi <alessandro.beneggi@studenti.unimi.it>
2023-12-02 15:02:11 +0000

    .

--- Sorgenti/prove.py
+++ Sorgenti/prove.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
 
 f = open("/workspace/VS/DataSet/Carbon dioxide (CO2) Emissions.csv", "r")
 line = f.readline()
@@ -10,37 +9,43 @@ while line != "":
         stati.append(elments[0])
     line = f.readline()
 f.close()
-#print(stati)
 
 f = open("/workspace/VS/DataSet/Life expectancy at birth.csv", "r")
 line = f.readline()
 nations = []
-y1990 = []
-y2000 = []
-y2012 = []
-prev = ""
 while line != "":
     elments = line.split(",")
     if elments[0] in stati and elments[2] == "Both sexes":    
         if elments[0] not in nations:
             nations.append(elments[0])
-        match elments[1]:
-            case "1990":
-                y1990.append(int(elments[3]))
-            case "2000":
-                y2000.append(int(elments[3]))
-            case "2012":
-                y2012.append(int(elments[3]))
     line = f.readline()
 f.close()
 
-print(len(stati))
-print(y1990)
-print(y2000)
-print(y2012)
-print(len(y2000))
-print(len(nations))
+years = []
+for i in range (1990, 2021, 1): 
+    years.append(i)
 
-
-df = pd.DataFrame({'1990': y1990, '2000': y2000, '2012': y2012}, index = nations )
-ax = df.plot.barh()
+f = open("/workspace/VS/DataSet/Carbon dioxide (CO2) Emissions.csv", "r")
+prev = ""
+val = []
+i=0
+line = f.readline()
+elments = line.split(",")
+while line != "":
+    prev = elments[0]
+    if elments[0] in nations:
+        while elments[0] == prev:
+            val.append(int(float(elments[2])/1000))
+            prev = elments[0]
+            line = f.readline()
+            elments = line.split(",")
+        plt.plot(years, val, label = elments[0])
+        val = []
+        i=i+1
+    else:
+        line = f.readline()
+        elments = line.split(",")
+f.close()
+print(i)
+plt.legend()
+plt.show() # BUG: manca australia
