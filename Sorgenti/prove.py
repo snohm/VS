7f17bde747b46f56b76e20ef5f5e371e3296a95e
Alessandro Beneggi <alessandro.beneggi@studenti.unimi.it>
2023-12-02 10:56:30 +0000

    .

--- Sorgenti/prove.py
+++ Sorgenti/prove.py
@@ -1,32 +1,46 @@
-#import pandas as pd
+import pandas as pd
+import seaborn as sns
 
 f = open("/workspace/VS/DataSet/Carbon dioxide (CO2) Emissions.csv", "r")
 line = f.readline()
 stati = []
-prev = ""
-i = 0
 while line != "":
     elments = line.split(",")
-    if elments[0] == prev:
-        line = f.readline()
-        continue
-    stati.append(elments[0])
-    prev = elments[0]
+    if elments[0] not in stati:
+        stati.append(elments[0])
     line = f.readline()
 f.close()
 #print(stati)
 
 f = open("/workspace/VS/DataSet/Life expectancy at birth.csv", "r")
 line = f.readline()
-data = []
+nations = []
+y1990 = []
+y2000 = []
+y2012 = []
+prev = ""
 while line != "":
     elments = line.split(",")
-    if elments[0] in stati:
-        #print(type(elments[2]))
-        if elments[2] == "Both sexes":
-            statesData = [elments[1], elments[0], elments[3]]
-            print(statesData)
-            data.append(statesData)
+    if elments[0] in stati and elments[2] == "Both sexes":    
+        if elments[0] not in nations:
+            nations.append(elments[0])
+        match elments[1]:
+            case "1990":
+                y1990.append(int(elments[3]))
+            case "2000":
+                y2000.append(int(elments[3]))
+            case "2012":
+                y2012.append(int(elments[3]))
     line = f.readline()
 f.close()
-print(data)
+
+print(len(stati))
+print(y1990)
+print(y2000)
+print(y2012)
+print(len(y2000))
+print(len(nations))
+
+
+df = pd.DataFrame({'1990': y1990, '2000': y2000, '2012': y2012}, index = nations )
+ax = df.plot.barh()
