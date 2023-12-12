024e8312c075638f4ffeb597be7f45f039476ce8
Alessandro Beneggi <alessandro.beneggi@studenti.unimi.it>
2023-12-12 22:24:04 +0000

    .

--- Sorgenti/Diseases.py
+++ Sorgenti/Diseases.py
@@ -4,7 +4,6 @@ def CoutnDeathPercentage():
     fH = open("/workspace/VS/DataSet/HivAfrica.csv", "r")
     fT = open("/workspace/VS/DataSet/TuberculosiAfrica.csv", "r")
     fP = open("/workspace/VS/DataSet/AfricanPopulation.csv", "r")
-
     val = []
     for i in range(0, 4, 1):
         LM = fM.readline()
@@ -16,7 +15,10 @@ def CoutnDeathPercentage():
                 LT = fT.readline()
                 elemT = LT.split(",")
                 valT = elemT[2].split(" ")
-                val.append(int(elemM[j]) + int(valT[0]))
+                LP = fP.readline()
+                elemP = LP.split(",")
+                valP = int(float(elemP[2])*10)
+                val.append((int(elemM[j]) + int(valT[0]))/valP)
         elif elemH[0] == "South Sudan":
             k = 0
             for j in range(1, len(elemH), 1):
@@ -24,22 +26,27 @@ def CoutnDeathPercentage():
                     LT = fT.readline()
                     elemT = LT.split(",")
                     valT = elemT[2].split(" ")
-                    val.append(int(elemH[j]) + int(elemM[j]) + int(valT[0]))
+                    LP = fP.readline()
+                    elemP = LP.split(",")
+                    valP = int(float(elemP[2])*10)
+                    val.append((int(elemH[j]) + int(elemM[j]) + int(valT[0]))/valP)
                 else:
-                    val.append(int(elemH[j]) + int(elemM[j]))
+                    LP = fP.readline()
+                    elemP = LP.split(",")
+                    valP = int(float(elemP[2])*10)
+                    val.append((int(elemH[j]) + int(elemM[j]))/valP)
                 k = k + 1         
         else:
             for j in range(1, len(elemH), 1):
                 LT = fT.readline()
                 elemT = LT.split(",")
                 valT = elemT[2].split(" ")
-                val.append(int(elemH[j]) + int(elemM[j]) + int(valT[0]))
-        #print(elemH[0])
-        #print(val)
-        #val = []
+                LP = fP.readline()
+                elemP = LP.split(",")
+                valP = int(float(elemP[2])*10)
+                val.append((int(elemH[j]) + int(elemM[j]) + int(valT[0]))/valP)
     fM.close()
     fH.close()
     fT.close()
     fP.close()
     return val
-CoutnDeath()    
