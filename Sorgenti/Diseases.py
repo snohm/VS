3ae3dea7c5b4a048dbb9ca308c85efdd3aa90c21
Alessandro Beneggi <alessandro.beneggi@studenti.unimi.it>
2023-12-18 14:43:41 +0000

    stacked version

--- Sorgenti/Diseases.py
+++ Sorgenti/Diseases.py
@@ -4,7 +4,10 @@ def CoutnCasesPercentage():
     fH = open("/workspace/VS/DataSet/HivAfricaCases.csv", "r")
     fT = open("/workspace/VS/DataSet/TuberculosiAfricaCases.csv", "r")
     fP = open("/workspace/VS/DataSet/AfricanPopulation.csv", "r")
-    val = []
+    risM = []
+    risH = []
+    risT = []
+    roundUP = 0.5
     for i in range(0, 4, 1):
         LM = fM.readline()
         LH = fH.readline()
@@ -19,7 +22,10 @@ def CoutnCasesPercentage():
                 elemP = LP.split(",")
                 valP = int(float(elemP[2])*10)
                 valM = elemM[j].split("[")
-                val.append(int((int(valM[0]) + int(valT[0]))/valP))
+                #val.append(int((int(valM[0]) + int(valT[0]))/valP))
+                risH.append(0)
+                risM.append(int(int(valM[0])/valP))
+                risT.append(int((int(valT[0])/valP)+roundUP))
         elif elemH[0] == "South Sudan":
             k = 0
             for j in range(1, len(elemH), 1):
@@ -32,14 +38,20 @@ def CoutnCasesPercentage():
                     valP = int(float(elemP[2])*10)
                     valM = elemM[j].split("[")
                     valH = elemH[j].split("[")
-                    val.append(int((int(valH[0]) + int(valM[0]) + int(valT[0]))/valP))
+                    #val.append(int((int(valH[0]) + int(valM[0]) + int(valT[0]))/valP))
+                    risH.append(int(int(valH[0])/valP))
+                    risM.append(int(int(valM[0])/valP))
+                    risT.append(int((int(valT[0])/valP)+roundUP))
                 else:
                     LP = fP.readline()
                     elemP = LP.split(",")
                     valP = int(float(elemP[2])*10)
                     valM = elemM[j].split("[")
                     valH = elemH[j].split("[")
-                    val.append(int((int(valH[0]) + int(valM[0]))/valP))
+                    #val.append(int((int(valH[0]) + int(valM[0]))/valP))
+                    risH.append(int(int(valH[0])/valP))
+                    risM.append(int(int(valM[0])/valP))
+                    risT.append(0)
                 k = k + 1         
         else:
             for j in range(1, len(elemH), 1):
@@ -51,9 +63,13 @@ def CoutnCasesPercentage():
                 valP = int(float(elemP[2])*10)
                 valM = elemM[j].split("[")
                 valH = elemH[j].split("[")
-                val.append(int((int(valH[0]) + int(valM[0]) + int(valT[0]))/valP))
+                #val.append(int((int(valH[0]) + int(valM[0]) + int(valT[0]))/valP))
+                risH.append(int(int(valH[0])/valP))
+                risM.append(int(int(valM[0])/valP))
+                risT.append(int((int(valT[0])/valP)+roundUP))
     fM.close()
     fH.close()
     fT.close()
     fP.close()
-    return val
+    return risH, risM, risT
+print(CoutnCasesPercentage())
